12b3afa1fd630d3c05239da223bacae66fd6c888
itc-s23010 <s23010@std.it-college.ac.jp>
2024-01-13 09:18:02 +0000

    rokumade

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
+import Meta from 'components/meta'
+import HeroComponent from 'components/hero'
 import Container from 'components/container'
-import Hero from 'components/hero'
 
-export default function Home () {
+// Use a different name for the constant
+const CustomHero = () => {
   return (
     <Container>
-      <Hero title='CUBE' subtitle='アウトプットしていくサイト' imageOn />
+      <Meta />
+      <HeroComponent
+        title='CUBE'
+        subtitle='アウトプットしていくサイト'
+        imageOn
+      />
     </Container>
   )
 }
+
+export default CustomHero

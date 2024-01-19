5cac2e0f3d3fd3435fe1fc59e17c9a2747de25b6
itc-s23010 <s23010@std.it-college.ac.jp>
2024-01-19 06:57:00 +0000

    instorusinaosi

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
-    domains: ['images.microcms-assets.io']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.microcms-assets.io',
+        port: '',
+        pathname: '/assets/*/*/*'
+      }
+    ]
   }
 }
-
 module.exports = nextConfig

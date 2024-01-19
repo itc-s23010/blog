303574b2a68469e41899d85a59c81747d5f1239a
itc-s23010 <s23010@std.it-college.ac.jp>
2024-01-19 06:02:09 +0000

    kyuumade

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,44 @@
-// Import with a different name
-import Meta from 'components/meta'
+import { getAllPosts } from 'lib/api'
 import HeroComponent from 'components/hero'
 import Container from 'components/container'
+import Posts from 'components/posts'
+import Pagination from 'components/pagination'
+import { getPlaiceholder } from 'plaiceholder'
+import { eyecatchLocal } from 'lib/constants'
 
 // Use a different name for the constant
-const CustomHero = () => {
+const CustomHero = ({ posts }) => {
   return (
     <Container>
-      <Meta />
       <HeroComponent
         title='CUBE'
         subtitle='アウトプットしていくサイト'
         imageOn
       />
+
+      <Posts posts={posts} />
+      <Pagination nextUrl='/blog' nextText='More Posts' />
     </Container>
   )
 }
 
 export default CustomHero
+
+const getStaticProps = async () => {
+  const posts = await getAllPosts(4)
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+export { getStaticProps }
